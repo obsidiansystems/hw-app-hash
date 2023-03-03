51bdf96d597b2aff15444aa7ee21e1613be99511
John Ericson <John.Ericson@Obsidian.Systems>
2023-03-03 10:45:54 -0500

    Use `hw-app-alamgu`, bump version

--- src/Provenance.ts
+++ src/Provenance.ts
@@ -15,7 +15,7 @@
  *  limitations under the License.
  ********************************************************************************/
 import type Transport from "@ledgerhq/hw-transport";
-import { Common, GetPublicKeyResult, SignTransactionResult, GetVersionResult } from "hw-app-obsidian-common";
+import { Common, GetPublicKeyResult, SignTransactionResult, GetVersionResult } from "hw-app-alamgu";
 
 export { GetPublicKeyResult, SignTransactionResult, GetVersionResult };
 
